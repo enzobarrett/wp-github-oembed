25c74e17b3e4366db818a661e7d2fb5482ffb9c6
Enzo Barrett <enzobarrett@gmail.com>
2020-04-12 01:29:16 -0600

    Embed contribution chart into user view

--- templates/author.php
+++ templates/author.php
@@ -7,5 +7,31 @@
 		</a>
 		<br>
 		<?php echo esc_html( number_format_i18n( $data['owner_info']->public_repos ) ) ?> repositories, <?php echo esc_html( number_format_i18n( $data['owner_info']->followers ) ) ?> followers.
+
+		<?php
+		# Use the Curl extension to query github and get back a page of results
+
+		$url = "https://github.com/users/" . $data['owner'] . "/contributions";
+
+		$ch = curl_init();
+
+		$timeout = 5;
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		$html = curl_exec($ch);
+		curl_close($ch);
+
+		# Create a DOM parser object
+		$dom = new DOMDocument();
+
+		@$dom->loadHTML($html);
+
+		# Find class that contains chart, and print that
+		$classname="position-relative";
+		$finder = new DomXPath($dom);
+		$spaner = $finder->query("//*[contains(@class, '$classname')]");
+		echo $dom->saveHTML($spaner[0]);
+		?>
 	</p>
 </div>
